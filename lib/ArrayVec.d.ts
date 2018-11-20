0677f31ec54b28cf51560349300118571dcbdc21
johtela <tommi.johtela@dnainternet.net>
2018-11-20 13:47:58 +0200

    Added .lib to ignore list.

--- lib/ArrayVec.d.ts
+++
@@ -1,4 +0,0 @@
-import { Vec2, Vec3, Vec4, NewVec } from "./Vectors";
-export declare const newVec2: NewVec<Vec2>;
-export declare const newVec3: NewVec<Vec3>;
-export declare const newVec4: NewVec<Vec4>;

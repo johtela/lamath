039409826c4a8baf3be22d6516a105a32cf37abd
johtela <tommi.johtela@dnainternet.net>
2020-03-28 20:55:16 +0200

    Exported more types

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import * as ArrayExt from './arrayext';
 import * as FMath from './fmath';
 import * as Vec from './vec';
 import * as Mat from './mat';
-import { Vector } from './vec';
-import { Matrix } from './mat';
+import { Vector, Vec2, Vec3, Vec4 } from './vec';
+import { Matrix, SquareMatrix, Mat2, Mat3, Mat4 } from './mat';
 
-export { ArrayExt, FMath, Vec, Mat, Vector, Matrix }
+export { ArrayExt, FMath, Vec, Mat, Vector, Vec2, Vec3, Vec4, 
+    Matrix, SquareMatrix, Mat2, Mat3, Mat4 }
